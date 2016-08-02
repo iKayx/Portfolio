e64702e7200abf7597ec207736b966d5a3d3f4ec
Marvin Kayx <Marvin.Quaty@Googlemail.com>
2016-08-02 12:26:31 +0200

    Delete Form1.vb

--- WindowsName/WindowsName/Form1.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class Form1
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
-        MsgBox("Benutzername: " + Environment.UserName)
-        Me.Close()
-    End Sub
-End Class
